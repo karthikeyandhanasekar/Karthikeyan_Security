f1abeea062f0435e783c64424abf3553f8d9851b
karthikeyan <karthikeyan@gmail.com>
2020-04-13 17:37:02 +0530

    final changes

---
+++ security/.dart_tool/flutter_build/8dc546cd5511a318a14ad0023d759de9/arm64-v8a/gen_snapshot.d
@@ -0,0 +1,1 @@
+gen_snapshot.d: D:\flutter\bin\cache\artifacts\engine\android-arm64-release\windows-x64\gen_snapshot
---
+++ security/build/flutter_open_whatsapp/generated/source/buildConfig/debug/infozenplus/com/flutter_open_whatsapp/BuildConfig.java
@@ -0,0 +1,18 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package infozenplus.com.flutter_open_whatsapp;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String LIBRARY_PACKAGE_NAME = "infozenplus.com.flutter_open_whatsapp";
+  /**
+   * @deprecated APPLICATION_ID is misleading in libraries. For the library package name use LIBRARY_PACKAGE_NAME
+   */
+  @Deprecated
+  public static final String APPLICATION_ID = "infozenplus.com.flutter_open_whatsapp";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = -1;
+  public static final String VERSION_NAME = "";
+}
